3971dfb33a5607b4f54a237ca7b6dc90bd398841
TermiT <web4@ivc.tshtt.uz>
2018-02-02 10:51:03 +0500

    added files

---
+++ environments/dev/common/config/params-local.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'cookieValidationKey' => 'MsnPKfNElbrUqNlPHo32pVztYl3-3wdH',
+    'cookieDomain' => '.ipak.dev',
+    'backendHostInfo' => 'http://backend.ipak.uz',
+    'frontendHostInfo' => 'http://ipak.uz',
+];
---
+++ environments/prod/common/config/params-local.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'cookieValidationKey' => 'MsnPKfNElbrUqNlPHo32pVztYl3-3wdH',
+    'cookieDomain' => '.brs.dev',
+    'backendHostInfo' => 'http://backend.brs.uz',
+    'frontendHostInfo' => 'http://brs.uz',
+];
