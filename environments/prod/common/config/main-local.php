e7b7ea191feb0eae3f6c7cb8c81c3e0618de6130
unknown <termit_90@mail.ru>
2018-03-16 15:10:54 +0500

    fixed

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
    'components' => [
       'db' => [
          'class' => 'yii\db\Connection',
-         'dsn' => 'mysql:host=localhost;dbname=db',
+         'dsn' => 'mysql:host=localhost;dbname=bank',
          'username' => 'root',
-         'password' => '',
+         'password' => '1z2x3c4v5b@#',
          'charset' => 'utf8',
           'attributes' => [
               PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));",
